27bf347cbdba9c4fbec5a3c866a64ea17190626e
Anthony Masi <amasijr@outlook.com>
2020-02-17 07:02:43 +0000

    Update main() to read from /proc/{pid}/stat

--- main.c
+++ main.c
@@ -7,21 +7,28 @@
 
 bool ispid(char name[]);
 
+struct process {
+  char *name;
+  char *pid;
+};
+
 int main() {
   DIR *procdir;
   FILE *fptr;
-  struct dirent *dirptr = (void *)0xDEC0DED;
-  
+  struct dirent *dirptr = (void *)0xAAAAAAAA;
+  struct process proc;
+
   const char *basepath = "/proc/";
   char *line = NULL;
-  char pname[64];
+  char ppidstr[6]; // max 32768 + null term
   char *token = NULL;
-  char  uid[4];
+  pid_t ppid;
   size_t len = 0;
   ssize_t read;
 
   procdir = opendir(basepath);
-  sprintf(uid, "%lu", (unsigned long int)getuid()); 
+  ppid = getppid();
+  sprintf(ppidstr, "%d", ppid);
 
   if (procdir == NULL) {
     printf("Can't open /proc");
@@ -34,12 +41,12 @@ int main() {
     if (dirptr == NULL) {
       break;
     }
-   
+
     if (ispid(dirptr->d_name)) {
-      char *path = malloc(strlen(basepath) + 1 + strlen(dirptr->d_name) + 7);
+      char *path = malloc(strlen(basepath) + strlen(dirptr->d_name) + 6);
       strcpy(path, basepath);
       strcat(path, dirptr->d_name);
-      strcat(path, "/status");
+      strcat(path, "/stat\0");
       
       fptr = fopen(path, "r");
       if (fptr == NULL) {
@@ -48,41 +55,40 @@ int main() {
       }
 
       int i;
+      bool filter;
       while ((read = getline(&line, &len, fptr)) != -1) {
-        if (strncmp("Name", line, 4) == 0) {
-          token = strtok(line, "\t");
-          for (i = 0; token != NULL; i++) {
-            if (!i) {
-              token = strtok(NULL, "\t");
-              continue;
-            }
-            
-            strcpy(pname, token);
-            token = strtok(NULL, "\t");
-          }
-        }
-        
-        if (strncmp("Uid", line, 3) == 0) {
-          token = strtok(line, "\t");
-          for (i = 0; token != NULL; i++) {
-            if (!i) {
-              token = strtok(NULL, "\t");
-              continue;
-            }
-         
-            if (strncmp(uid, token, strlen(uid)) == 0) {
-              printf("%s\t%s", dirptr->d_name, pname);
+        token = strtok(line, " ");
+        filter = false;
+        for (i = 0; token != NULL; i++) {
+          switch(i) {
+            case 0:
+              proc.pid = token;
+              if (strcmp(token, ppidstr) == 0) {
+                filter = true;
+              }
               break;
-            } 
-            token = strtok(NULL, "\t");
+            case 1:
+              proc.name = token;
+              break;
+            case 3:
+              if (strcmp(token, ppidstr) == 0) {
+                filter = true;
+              }
+              break;
+            default:
+              break;
           }
+          token = strtok(NULL, " ");
         }
+      }
+
+      if (filter) {
+        printf("%s\t%s\n", proc.pid, proc.name);
       }
        
       fclose(fptr);
     } 
   }
-
   closedir(procdir);
   return 0;
 }
